4e267f7edcd95eebadf1c6d7aaeee7cbedb309f9
acrazyczy <a_crazy_czy@yahoo.com>
2021-01-02 22:43:16 +0800

    remove unnecessary files and update README.md

--- serial/build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/crazy_cloud/Desktop/Foundation/serial/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
